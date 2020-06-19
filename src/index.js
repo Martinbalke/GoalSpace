298034b156051552b1dd8aa1bb86dd92c0405621
Martin Balke <Mrtnbalke@gmail.com>
2020-06-19 14:59:33 -0700

    even more initial setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './src/App';
+import { Provider } from 'react-redux'
+import configureStore from './store';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={configureStore()}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
