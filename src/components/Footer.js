5c01c9ccfc4a46fed68765101a44106cf386a82d
Martin Balke <Mrtnbalke@gmail.com>
2020-06-21 19:15:32 -0700

    basic header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,10 @@ import github from '../images/github.png'
 function Footer(props) {
 
   return (<div className='footer'>
+
+    <p className="footer__copy">
+      Goalspace is &copy; 2020 Martin Balke 
+    </p>
     <div className="footer__socials">
       <a href="https://www.linkedin.com/in/martinbalke/" className="footer_social" target="_blank" rel='noopener noreferrer'><img src={linked} alt='A link to the authors linked in'/></a>
       <a href="https://github.com/Martinbalke" className="footer_social" target="_blank" rel='noopener noreferrer'><img src={github} alt="A link to the authors github"/></a>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,9 @@ function Hero(props) {
   return (<div className='hero'>
     <h2 className='hero__header'>GoalSpace</h2>
     <p className='hero__subtext'>Take the work out of working towards your goals</p>
-    <button className='hero__btn'/>
+      {/* <a href='#'>
+        <svg className='hero__svg' viewBox="0 0 100 100"  xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M23.245 20l-11.245-14.374-11.219 14.374-.781-.619 12-15.381 12 15.391-.755.609z"/></svg>
+      </a> */}
   </div>);
 }
 
