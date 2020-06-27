e8bc0719ec59637094a8521594d2f6b98d3f59a8
Martin Balke <Mrtnbalke@gmail.com>
2020-06-27 15:51:25 -0700

    popup styling

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -30,48 +30,51 @@ function Popup({ close, dispatch, index, goals }) {
 
   useEffect(() => {
     if (index !== undefined && goals[index]) setGoal(goals[index]);
-  },[])
+  }, [])
 
 
 
   return (
     <div className='popup' ref={popupRef}>
-      <form onSubmit={(e) => {
+  
+      <form className="popup__form" onSubmit={(e) => {
         e.preventDefault();
         dispatch({ type: 'NEW_GOAL', goal, index })
         close();
-      }}
-        className="popup__form">
-        <button className='btn btn--close popup__btn' onClick={close}>close</button>
+      }}>
+        <button className='btn btn--close popup__close' onClick={close}/>
+        <h3 className='popup__tertiary'>Set a new goal</h3>
         <label htmlFor="yearly" className="popup__yearly">
           Yearly Goal
-      <input type="text" className="popup__yearly-input" id='yearly' required defaultValue={goal.yearly} onChange={(e) => {
+          <input type="text" className="popup__yearly-input" id='yearly' required defaultValue={goal.yearly} onChange={(e) => {
             e.preventDefault();
             setGoal({ ...goal, yearly: e.target.value })
           }} />
         </label>
-        <label htmlFor="habit1" className="popup__habit1">
-          Habit 1
-        <input type="text" className="popup__habit1-input" id='habit1' required defaultValue={goal.habit1} onChange={(e) => {
-            e.preventDefault();
-            setGoal({ ...goal, habit1: e.target.value })
-          }} />
-        </label>
-        <label htmlFor="habit2" className="popup__habit2" >
-          Habit 2
-        <input type="text" className="popup__habit2-input" id='habit2' required defaultValue={goal.habit2} onChange={(e) => {
-            e.preventDefault();
-            setGoal({ ...goal, habit2: e.target.value })
-          }} />
-        </label>
-        <label htmlFor="habit3" className="popup__habit3">
-          Habit 3
-        <input type="text" className="popup__habit3-input" id='habit3' required defaultValue={goal.habit3} onChange={(e) => {
-            e.preventDefault();
-            setGoal({ ...goal, habit3: e.target.value })
-          }} />
-        </label>
-        <button type='submit' className='btn btn--close popup__btn'>Submit</button>
+        <div className="popup__habits">
+          <label htmlFor="habit1">
+            Habit 1
+            <input type="text" id='habit1' required defaultValue={goal.habit1} onChange={(e) => {
+              e.preventDefault();
+              setGoal({ ...goal, habit1: e.target.value })
+            }} />
+          </label>
+          <label htmlFor="habit2">
+            Habit 2
+            <input type="text" id='habit2' required defaultValue={goal.habit2} onChange={(e) => {
+              e.preventDefault();
+              setGoal({ ...goal, habit2: e.target.value })
+            }} />
+          </label>
+          <label htmlFor="habit3">
+            Habit 3
+            <input type="text" id='habit3' required defaultValue={goal.habit3} onChange={(e) => {
+              e.preventDefault();
+              setGoal({ ...goal, habit3: e.target.value })
+            }} />
+          </label>
+        </div>
+        <button type='submit' className='btn popup__submit' />
       </form>
 
     </div>);
--- src/components/Yearly.js
+++ src/components/Yearly.js
@@ -8,7 +8,7 @@ function Yearly({ goals, setEditing }) {
     return goals.map((goal, index) => (
       <div className="yearly__goal" key={index}>
         <h3 className='yearly__tertiary'>{goal.yearly}</h3>
-        <button className='btn yearly__btn' onClick={() => { setEditing(index) }}>test</button>
+        <button className='btn yearly__btn' onClick={() => { setEditing(index) }}></button>
       </div>
 
     ));
@@ -18,7 +18,7 @@ function Yearly({ goals, setEditing }) {
     <div className="yearly">
       <div className="border"/>
       <h3 className='yearly__tertiary'>Create a new yearly goal</h3>
-      <button className='btn yearly__btn--main' onClick={() => setEditing(goals.length)}>click</button>
+      <button className='btn yearly__btn--main' onClick={() => setEditing(goals.length)}></button>
       {yearlyGoals()}
     </div>
   )
