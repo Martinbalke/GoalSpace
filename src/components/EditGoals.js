9cf1f28a2df7153be599fb5d77651ebfbb4f44fe
Martin Balke <Mrtnbalke@gmail.com>
2020-07-10 15:48:17 -0700

    refactor yearly into two components and added a text animation

---
+++ src/components/EditGoals.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+
+function EditGoals({goals, setEditing}) {
+    if (!goals.length) return <div></div>
+    return (
+      <div className="edit">
+        <h3 className="edit__tertiary">Edit your goals :</h3>
+        {goals.map((goal, index) => (
+          <div className="edit__goal" key={index}>
+            <h3 className='edit__tertiary'>{goal.goal}</h3>
+            <button className='btn edit__btn' onClick={() => { setEditing(index) }}></button>
+          </div>))}
+      </div>
+    );
+  }
+const mSTP = state => ({
+  goals: state.goals
+})
+
+export default connect(mSTP)(EditGoals);
--- src/components/GoalForm.js
+++ src/components/GoalForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useLayoutEffect } from 'react';
+import React, { useState } from 'react';
 import Input from './Input';
 import { connect } from 'react-redux';
 import { newGoal, updateGoal } from '../store/goalReducer';
@@ -34,9 +34,9 @@ function GoalForm({ close, dispatch, index, goals }) {
             goal._id ? dispatch(updateGoal(goal, index)) : dispatch(newGoal(goal, index));
             close();
           }}>
-          <button className='btn btn--close popup__close' onClick={close} />
+          <button className='btn btn--close goalForm__close' onClick={close} />
           <h3 className='goalForm__tertiary'>Set a new goal</h3>
-          <div className="goalForm__inputs">
+          <div className="goalForm__inputs-container">
             <Input className='goalForm__input' id='goal' callback={(text) => inputTextChange(text, -1)} defaultValue={goal.goal} labelText='Goal' />
             <Input className='goalForm__input' id='habit1' callback={(text) => { inputTextChange(text, 0) }} defaultValue={goal.habits[0]} labelText='Habit 1' />
             <Input className='goalForm__input' id='habit2' callback={(text) => { inputTextChange(text, 1) }} defaultValue={goal.habits[1]} labelText='Habit 2' />
@@ -46,7 +46,7 @@ function GoalForm({ close, dispatch, index, goals }) {
         </form>
       </motion.div>
       <motion.div className="goalForm__sidebar" variants={popupFormAnimation}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className='popup__sidebar-svg'>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className='goalForm__sidebar-svg'>
           <path fillOpacity="1" d="M0,96L40,112C80,128,160,160,240,176C320,192,400,192,480,192C560,192,640,192,720,197.3C800,203,880,213,960,224C1040,235,1120,245,1200,218.7C1280,192,1360,128,1400,96L1440,64L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path>
         </svg>
         <h3 className="goalForm__sidebar-tertiary">Creating a meaningful goal</h3>
