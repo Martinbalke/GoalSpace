1e4d4295014dccafc4da6e97847c55ae19f41e2a
Martin Balke <Mrtnbalke@gmail.com>
2020-07-28 15:01:18 -0700

    basic nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,19 +4,16 @@ import React from 'react';
 function Nav() {
   return (
   <div class="navigation">
-      <input type="checkbox" class="navigation__checkbox" id="navi-toggle"/>
-
-      <label for="navi-toggle" class="navigation__button">
+      <input type="checkbox" class="navigation__checkbox" id="nav-check" />
+      <label for="nav-check" class="navigation__button">
           <span class="navigation__icon">&nbsp;</span>
       </label>
-
       <div class="navigation__background">&nbsp;</div>
-
       <nav class="navigation__nav">
           <ul class="navigation__list">
-              <li class="navigation__item"><a href="#chart" class="navigation__link"><span>01</span>Login</a></li>
-              <li class="navigation__item"><a href="#chart" class="navigation__link"><span>02</span>Goals</a></li>
-              <li class="navigation__item"><a href="#chart" class="navigation__link"><span>03</span>Contact Us</a></li>
+              <li class="navigation__item"><a href="#chart" class="navigation__link">Login</a></li>
+              <li class="navigation__item"><a href="#chart" class="navigation__link">Goals</a></li>
+              <li class="navigation__item"><a href="#chart" class="navigation__link">Contact</a></li>
               </ul>
       </nav>
   </div>
