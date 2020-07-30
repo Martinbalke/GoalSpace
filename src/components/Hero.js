90cbb812a9b3be8a3e7368687ff1d98eec1cdc1e
Martin Balke <Mrtnbalke@gmail.com>
2020-07-30 00:53:54 -0700

    fail

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ function Hero(props) {
         <div className="hero__bubbles">
           <div /><div /><div /><div /><div /><div />
         </div>
-        <span className='hero__header-logo'>&nbsp;</span>
+        <span className='hero__header-logo'>GoalSpace</span>
         <span className='hero__header-text-1'>Set a meaningful goal</span>
         <br></br>
         <span className='hero__header-text-2'>Accomplish it with daily habits</span>
