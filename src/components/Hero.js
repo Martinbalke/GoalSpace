c23f080718eb0a7a9839ca4343533d17084967e3
Martin Balke <Mrtnbalke@gmail.com>
2020-07-30 00:38:01 -0700

    fail

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import { motion } from "framer-motion"
+import {} from './animations'
 function Hero(props) {
 
   return (
     <section className='hero'>
-      <h1 className='hero__header'>
+      <motion.h1 className='hero__header'>
         <div className="hero__bubbles">
           <div /><div /><div /><div /><div /><div />
         </div>
-        <span className='hero__header-logo'>&nbsp;</span>
-        <span className='hero__header-text-1'>Set a meaningful goal</span>
+        <span className='hero__header-logo'>GoalSpace</span>
+        <span className='hero__header-text-1'>Bring meaning back to your goals</span>
         <br></br>
-        <span className='hero__header-text-2'>Accomplish it with daily habits</span>
-      </h1>
+        <span className='hero__header-text-2'>Track daily progess, complete milestones, and finish goals</span>
+      </motion.h1>
       <div className="hero__shapes" />
     </section>);
 }
