42292e12f209f3c0ed514ecbbe54e1f598dbe8f6
Martin Balke <Mrtnbalke@gmail.com>
2020-08-07 19:06:54 -0700

    added buttons to the hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,6 +10,10 @@ function Hero(props) {
         <span className='hero__header-logo'>Goalspace</span>
         <span className='hero__header-text-1'>From daily <em>habits</em> to finished <em>goals</em></span>
       </h1>
+      <div className="hero__buttons">
+        <button className='btn btn-hero btn-hero--outline'>Learn more</button>
+        <button className='btn btn-hero btn-hero--solid'>Login</button>
+      </div>
     </section>);
 }
 
