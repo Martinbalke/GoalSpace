e0cdee44b37d1fe498926457abad81504f808bdb
Martin Balke <Mrtnbalke@gmail.com>
2020-08-12 15:21:11 -0700

    about section open/close functionality and styling

---
+++ src/components/About.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {motion} from 'framer-motion'
+import {popupContainerAnimation} from './animations'
+
+
+export default ({ callback }) => {
+
+  return (
+    <motion.div className="about" variants={popupContainerAnimation} initial='hidden' animate='visible' exit='exit' >
+      <h3 className="about__header">About Goalspace</h3>
+      <button type='button' className='btn btn-round about__close' onClick={() => callback(false)}/>
+      <div className="about__sections">
+        <div className="about__section">
+          <h3 className="about__section-tertiary">Set Goals</h3>
+          <p className="about__section-paragraph">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi quas dicta ducimus incidunt ea alias maiores quos beatae quis, animi eum assumenda, voluptatibus, accusamus eligendi labore? Repellendus quos tenetur ullam.</p>
+        </div>
+        <div className="about__section">
+          <h3 className="about__section-tertiary">Habits</h3>
+          <p className="about__section-paragraph">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi quas dicta ducimus incidunt ea alias maiores quos beatae quis, animi eum assumenda, voluptatibus, accusamus eligendi labore? Repellendus quos tenetur ullam.</p>
+        </div>
+        <div className="about__section">
+          <h3 className="about__section-tertiary">Milestone</h3>
+          <p className="about__section-paragraph">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi quas dicta ducimus incidunt ea alias maiores quos beatae quis, animi eum assumenda, voluptatibus, accusamus eligendi labore? Repellendus quos tenetur ullam.</p>
+        </div>
+      </div>
+    </motion.div>
+
+  )
+}
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import About from './About';
+import { AnimatePresence } from 'framer-motion'
 function Hero(props) {
-
+  const [showAbout, setShowAbout] = useState(false);
   return (
-    <section className='hero'>
-      <h1 className='hero__header'>
-        <div className="hero__bubbles">
-          <div /><div /><div /><div /><div /><div />
+    <>
+      <section className='hero'>
+        <h1 className='hero__header'>
+          <div className="hero__bubbles">
+            <div /><div /><div /><div /><div /><div />
+          </div>
+          <span className='hero__header-logo'>Goalspace</span>
+          <span className='hero__header-text-1'>From daily <em>habits</em> to finished <em>goals</em></span>
+        </h1>
+        <div className="hero__buttons">
+          <button className='btn btn-hero btn-hero--outline' onClick={() => setShowAbout(true)}>Learn more</button>
+          <button className='btn btn-hero btn-hero--solid'>Login</button>
         </div>
-        <span className='hero__header-logo'>Goalspace</span>
-        <span className='hero__header-text-1'>From daily <em>habits</em> to finished <em>goals</em></span>
-      </h1>
-      <div className="hero__buttons">
-        <button className='btn btn-hero btn-hero--outline'>Learn more</button>
-        <button className='btn btn-hero btn-hero--solid'>Login</button>
-      </div>
-    </section>);
+      </section>
+      {showAbout && (
+        <AnimatePresence>
+          <About callback={setShowAbout} />
+        </AnimatePresence>
+      )}
+    </>
+  );
 }
 
 
